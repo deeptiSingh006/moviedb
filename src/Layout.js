72f0c266c4e338c91db5119e0a220d95cbcace48
Deepti Singh <deepti.m.singh@gmail.com>
2022-05-04 22:24:03 +0530

    navbar using navlink isactive property

--- src/Layout.js
+++ src/Layout.js
@@ -1,21 +1,25 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
+
+/*<li className="p-4 mx-2" >
+            <NavLink style={({ isActive }) => 
+                      (isActive ? {color: 'red'} : {color: 'blue'})}  to="/cardgame">Card Game</NavLink>     isActive is provided with navlink based on that */
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Layout</Link>
+      <nav className=" relative bg-gray-400 mb-3 text-red-100 ">
+        <ul  className="relative flex flex-wrap items-center justify-start px-2 py-3 ">
+          <li className="p-4 mx-2">
+            < NavLink  className={({ isActive }) => (isActive ? "rounded-full border-indigo-500/100 bg-green-500 px-2" : "rounded-full border-indigo-500/100 bg-blue-500 px-2")} to="/">Layout</NavLink>
           </li>
-          <li>
-            <Link to="/MovieDb">Movies Db</Link>
+          <li className="p-4 mx-2">
+            <NavLink className={({ isActive }) => (isActive ? "rounded-full border-indigo-500/100 bg-green-500 px-2" : "rounded-full border-indigo-500/100 bg-blue-500 px-2")} to="/MovieDb">Movies Db</NavLink>
           </li>
-          <li>
-            <Link to="/cardgame">Card Game</Link>
+          <li className="p-4 mx-2" >
+            <NavLink className={({ isActive }) => (isActive ? "rounded-full border-indigo-500/100 bg-green-500 px-2" : "rounded-full border-indigo-500/100 bg-blue-500 px-2")}  to="/cardgame">Card Game</NavLink>
           </li>
-          <li>
-            <Link to="/toss">Toss Game</Link>
+          <li className="p-4 mx-2">
+            <NavLink className={({ isActive }) => (isActive ? "rounded-full border-indigo-500/100 bg-green-500 px-2" : "rounded-full border-indigo-500/100 bg-blue-500 px-2")} to="/toss">Toss Game</NavLink>
           </li>
 
 
--- src/MovieDbApp/MovieDb.js
+++ src/MovieDbApp/MovieDb.js
@@ -2,8 +2,8 @@
 import SearchMovie from './SearchMovie';
 function MovieDb() {
   return (
-    <div className="container">
-      <h1 className="title"> React Movie App by DEEPTI !</h1>
+    <div className=" container px-4 mx-auto flex flex-col flex-wrap items-center justify-between">
+      <h1 > React Movie App by DEEPTI !</h1>
       <SearchMovie/>
     </div>
   );
